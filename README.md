e966491c78cf23a7438570d0e9843415ecbf2f97
454270186 <sc21ey@leeds.ac.uk>
2023-01-21 13:20:40 +0800

    a simple REPL

---
+++ README.md
@@ -0,0 +1,16 @@
+# A simple database demo for **Sqlilite**
+
+## Design of my Sqlite demo
+The **front-end** consists of:
+- tokenizer
+- parser
+- code generator
+
+The **back-end** consists of:
+- virtual machine
+- B-tree
+- pager
+- os interface
+
+## A simple REPL
+![Test](./img/repl_test.png)
---
+++ repl.c
@@ -0,0 +1,62 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_buffer;
+} InputBuffer;
+
+// Initialize a input buffer
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_buffer = 0;
+
+    return input_buffer;
+}
+
+// Prints a prompt to users
+void print_prompt() { printf("xiaofeiDB > "); }
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input.");
+        exit(EXIT_FAILURE);
+    }
+
+    // ignore the '\n'
+    input_buffer->input_buffer = bytes_read-1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+// destructs input buffer
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+    printf("Start...\n");
+
+    while(true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if(strcmp(".exit", input_buffer->buffer) == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
+
+    return 0;
+}
